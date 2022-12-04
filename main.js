f59615a75924214d9ab3f872f1276c2fd4d96378
Maks Skomorokha <91227382+connars@users.noreply.github.com>
2022-12-04 06:24:43 +0200

    main

---
+++ main.js
@@ -0,0 +1,4 @@
+document.querySelector('#rc-phone-icon').addEventListener("click", function(){   
+    this.classList.toggle('open');
+    document.getElementById('rc-phone').classList.toggle('open');
+});
